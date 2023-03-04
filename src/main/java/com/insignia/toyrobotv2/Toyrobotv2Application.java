9ff7097cedc32262b7d20084968165ae53c357a8
rangasanjuau <sanjay.ranga.au@gmail.com>
2023-03-05 01:14:44 +1100

    Initial Commit

---
+++ src/main/java/com/insignia/toyrobotv2/Toyrobotv2Application.java
@@ -0,0 +1,13 @@
+package com.insignia.toyrobotv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyrobotv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toyrobotv2Application.class, args);
+	}
+
+}
